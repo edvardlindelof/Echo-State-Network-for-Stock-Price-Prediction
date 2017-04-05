47a8737d7c042de2d941bb828d74d16443781146
Edvard Lindelöf <edvard@Bulldozer2.BulldozerHome>
2017-04-05 19:48:35 +0200

    training_iteration and corresponding test

--- esn.py
+++ esn.py
@@ -9,14 +9,21 @@ class EchoStateNetwork():
         m = y.shape[0]
         return np.sqrt(np.sum((y - y_target) ** 2) / m)
 
-    def _x_next_tilde(self, W_in, u, W, x):
+    def _x_tilde(self, W_in, u, W, x):
         u_star = np.vstack([[1], u])  # 1 prepended for bias
         return np.tanh(np.dot(W_in, u_star) + np.dot(W, x))
 
     def _x_next(self, alpha, x, x_tilde):
         return (1 - alpha) * x + alpha * x_tilde
 
-    def _y(self, W_out, u, x):  # assumes one-dimensional output
-        x_star = np.vstack([[1], u, x])  # 1 prepended for bias
-        return np.dot(W_out, x_star)[0,0]
+    def _z(self, u, x):
+        return np.vstack([[1], u, x])  # 1 prepended for bias
 
+    def _y_scalar(self, W_out, z):
+        return np.dot(W_out, z)[0,0]
+
+    def _training_iteration(self, W_in, u, W, x, alpha):
+        x_tilde = self._x_tilde(W_in, u, W, x)
+        x_next = self._x_next(alpha, x, x_tilde)
+        z = self._z(u, x_next)
+        return x, z
--- test_esn.py
+++ test_esn.py
@@ -14,12 +14,12 @@ class TestEchoStateNetwork(unittest.TestCase):
         by_hand = 1.6583124
         self.assertAlmostEqual(by_class, by_hand)
 
-    def test_x_next_tilde(self):
+    def test_x_tilde(self):
         W_in = np.array([[1,2,3],[4,5,6]])
         u = np.array([2, 2.5]).reshape(-1, 1)
         W = np.array([[1,1],[1,1]])
         x = np.array([-0.5, 0]).reshape(-1, 1)
-        by_class = self.esn._x_next_tilde(W_in, u, W, x)
+        by_class = self.esn._x_tilde(W_in, u, W, x)
         by_hand = np.tanh(np.array([12.0, 28.5]).reshape(-1, 1))
         self.assertTrue(np.allclose(by_class, by_hand))
 
@@ -31,13 +31,24 @@ class TestEchoStateNetwork(unittest.TestCase):
         by_hand = np.array([-3.0, 2]).reshape(-1, 1)
         self.assertTrue(np.allclose(by_class, by_hand))
 
-    def test_y(self):
+    def test_y_scalar(self):
         W_out = np.ones((1, 4))
-        u = np.array([0.5]).reshape(-1 ,1)
-        x = np.array([-1, 3]).reshape(-1, 1)
-        by_class = self.esn._y(W_out, u, x)
+        z = np.array([1,0.5,-1,3]).reshape(-1, 1)
+        by_class = self.esn._y_scalar(W_out, z)
         by_hand = 3.5
         self.assertEqual(by_class, by_hand)
+
+    def test_training_iteration(self):
+        W_in = np.random.rand(4, 3)
+        u = np.random.rand(2, 1)
+        W = np.random.rand(4, 4)
+        x = np.random.rand(4, 1)
+        alpha = 0.5
+        x_next, z = self.esn._training_iteration(W_in, u, W, x, alpha)
+        self.assertEqual(x_next.shape[0], 4)
+        self.assertEqual(x_next.shape[1], 1)
+        self.assertEqual(z.shape[0], 7)
+        self.assertEqual(z.shape[1], 1)
 
 if __name__ == '__main__':
     unittest.main()
