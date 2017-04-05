e3ee25a6c01c2750ca18778941fb9c258361fffe
Edvard Lindelöf <edvard@Bulldozer2.BulldozerHome>
2017-04-05 18:45:02 +0200

    propagation methods and corresponding tests

--- esn.py
+++ esn.py
@@ -8,3 +8,15 @@ class EchoStateNetwork():
     def _rms_error(self, y, y_target):
         m = y.shape[0]
         return np.sqrt(np.sum((y - y_target) ** 2) / m)
+
+    def _x_next_tilde(self, W_in, u, W, x):
+        u_star = np.vstack([[1], u])  # 1 prepended for bias
+        return np.tanh(np.dot(W_in, u_star) + np.dot(W, x))
+
+    def _x_next(self, alpha, x, x_tilde):
+        return (1 - alpha) * x + alpha * x_tilde
+
+    def _y(self, W_out, u, x):  # assumes one-dimensional output
+        x_star = np.vstack([[1], u, x])  # 1 prepended for bias
+        return np.dot(W_out, x_star)[0,0]
+
--- test_esn.py
+++ test_esn.py
@@ -11,7 +11,32 @@ class TestEchoStateNetwork(unittest.TestCase):
         y = np.array([1, 1.5, 0]).reshape(-1, 1)
         y_target = np.array([-1, -0.5, 0.5]).reshape(-1, 1)
         by_class = self.esn._rms_error(y, y_target)
-        by_hand = 1.6583123951776999
+        by_hand = 1.6583124
+        self.assertAlmostEqual(by_class, by_hand)
+
+    def test_x_next_tilde(self):
+        W_in = np.array([[1,2,3],[4,5,6]])
+        u = np.array([2, 2.5]).reshape(-1, 1)
+        W = np.array([[1,1],[1,1]])
+        x = np.array([-0.5, 0]).reshape(-1, 1)
+        by_class = self.esn._x_next_tilde(W_in, u, W, x)
+        by_hand = np.tanh(np.array([12.0, 28.5]).reshape(-1, 1))
+        self.assertTrue(np.allclose(by_class, by_hand))
+
+    def test_x_next(self):
+        alpha = 0.5
+        x = np.array([1,2]).reshape(-1, 1)
+        x_tilde = np.array([-7,2]).reshape(-1, 1)
+        by_class = self.esn._x_next(alpha, x, x_tilde)
+        by_hand = np.array([-3.0, 2]).reshape(-1, 1)
+        self.assertTrue(np.allclose(by_class, by_hand))
+
+    def test_y(self):
+        W_out = np.ones((1, 4))
+        u = np.array([0.5]).reshape(-1 ,1)
+        x = np.array([-1, 3]).reshape(-1, 1)
+        by_class = self.esn._y(W_out, u, x)
+        by_hand = 3.5
         self.assertEqual(by_class, by_hand)
 
 if __name__ == '__main__':
