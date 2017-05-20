cb8c50c12ad92c47af5d6606673e9bee68769c51
Edvard Lindelöf <edvard@Bulldozer2.BulldozerHome>
2017-05-20 13:12:30 +0200

    3rd model in pred_sml, without reservoir

--- pred_sml.py
+++ pred_sml.py
@@ -23,7 +23,7 @@ print(data_arr.std(axis=0))
 data_arr_normed = (data_arr - data_arr.mean(axis=0)) / (data_arr.std(axis=0) + 0.001)  # + 0.001 is ugly haxx
 data_arr = data_arr_normed
 
-n_test = 1000
+n_test = 100
 X = data_arr[:, 1:]
 y = data_arr[:, 0]
 
@@ -61,7 +61,7 @@ print(data_arr.std(axis=0))
 data_arr_normed = (data_arr - data_arr.mean(axis=0)) / (data_arr.std(axis=0) + 0.001)  # + 0.001 is ugly haxx
 data_arr = data_arr_normed
 
-n_test = 1000
+n_test = 100
 X = data_arr[:, 1:]
 y = data_arr[:, 0]
 
@@ -85,4 +85,43 @@ Z_train = esn.Z_train
 plt.plot(y_test)
 plt.plot(y_pred)
 #plt.plot(Z_train[0:300, 30:35])
+plt.show()
+
+
+##
+## USING AGGREGATED FEATURES, RIDGE REGRESSION (no reservoir)
+##
+
+raw_data = np.genfromtxt('SML_data_1_aggregated', delimiter=', ')
+
+data_list = raw_data
+data_arr = np.array(data_list)
+print(data_arr.std(axis=0))
+data_arr_normed = (data_arr - data_arr.mean(axis=0)) / (data_arr.std(axis=0) + 0.001)  # + 0.001 is ugly haxx
+data_arr = data_arr_normed
+
+n_test = 100
+X = data_arr[:, 1:]
+y = data_arr[:, 0]
+
+X_train = X[0:-n_test]
+y_train = y[0:-n_test]
+X_test = X[-n_test:-1]
+y_test = y[-n_test:-1]
+
+
+resolution = 5
+alpha = 0.5
+beta = 1e1
+first_column_amplifier = 1
+
+# parameters should in practice be like having no reservoir at all
+esn = EchoStateNetwork(2, alpha, beta, first_column_amplifier, density_W=1.)
+esn.fit(X_train, y_train)
+y_pred = esn.predict(X_test)
+
+Z_train = esn.Z_train
+
+plt.plot(y_test)
+plt.plot(y_pred)
 plt.show()
